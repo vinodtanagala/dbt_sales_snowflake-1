d83155637a9f299bc796a6b4911a4241743eb78f
vh vv <resumeproject11@gmail.com>
2026-09-06 05:36:24 +0000

    changes done to  dbt-deploy files and generate schema

--- macros/generate_custom_schema.sql
+++ macros/generate_custom_schema.sql
@@ -1,14 +1,21 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
-
+    {# For QA, PROD, or deployment targets, write directly to the specified custom schema #}
+    {%- if target.name in ['qa', 'qa_testing', 'prod'] -%}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
+        {# For DEV/sandbox runs, isolate schemas by prefixing with the default schema #}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- endif -%}
 
 {%- endmacro %}
